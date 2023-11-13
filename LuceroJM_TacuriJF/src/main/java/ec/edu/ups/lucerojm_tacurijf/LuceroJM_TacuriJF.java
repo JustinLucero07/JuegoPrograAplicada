717ce593a5f2ca75de9d71f30741729cb29cc088
JustinLucero07 <justinluceroxd04@gmail.com>
2023-11-13 11:52:22 -0500

    Mi parte(Justin)

---
+++ LuceroJM_TacuriJF/src/main/java/ec/edu/ups/lucerojm_tacurijf/LuceroJM_TacuriJF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ups.lucerojm_tacurijf;
+
+/**
+ *
+ * @author Usuario
+ */
+public class LuceroJM_TacuriJF {
+
+    public static void main(String[] args) {
+        System.out.println("Hola!");
+    }
+}
